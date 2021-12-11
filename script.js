83c3f4e8fcde7999503247cbf8f6636bb7f385e2
krowker <krowker97@gmail.com>
2021-12-11 18:36:42 +0300

    add cleartimeout

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
 let users = [
    {name: '1', atrChance: 100},
-   {name: '2', atrChance: 100},
-   {name: '3', atrChance: 100},
-   {name: '4', atrChance: 100},
-   {name: '5', atrChance: 100},
-   {name: '6', atrChance: 100},
-   {name: '7', atrChance: 100},
-   {name: '8', atrChance: 100},
-   {name: '9', atrChance: 100},
-   {name: '10', atrChance: 100},
-   {name: '11', atrChance: 100},
-   {name: '12', atrChance: 100},
-   {name: '13', atrChance: 100},
-   {name: '14', atrChance: 100},
+//    {name: '2', atrChance: 100},
+//    {name: '3', atrChance: 100},
+//    {name: '4', atrChance: 100},
+//    {name: '5', atrChance: 100},
+//    {name: '6', atrChance: 100},
+//    {name: '7', atrChance: 100},
+//    {name: '8', atrChance: 100},
+//    {name: '9', atrChance: 100},
+//    {name: '10', atrChance: 100},
+//    {name: '11', atrChance: 100},
+//    {name: '12', atrChance: 100},
+//    {name: '13', atrChance: 100},
+//    {name: '14', atrChance: 100},
 ]
 
 // for (let i = 0; i <users.length; i++) {
 //    console.log(users[i]);
 // }
+var firstInc
 
 function decAtrChance (index) {
     users[index].atrChance = users[index].atrChance - 10
 
-    setTimeout(()=> incAtrChance(index), 1000)
+
+    stopTimeout(firstInc)
+    firstInc = setTimeout(()=> incAtrChance(index), 6000)
+}
+
+function stopTimeout (timoutID) {
+    clearTimeout(timoutID)
 }
 
 function incAtrChance (index) {
-    users[index].atrChance = users[index].atrChance + 5
-    console.log(users[index].atrChance);
+    users[index].atrChance = users[index].atrChance + 10
+    console.log('after inc '+users[index].atrChance);
 }
 
 function getRandomUser () {
@@ -39,13 +46,11 @@ function getRandomUser () {
     index = Math.floor(Math.random()*(max-min)+min);
     compareAtr = Math.floor(Math.random()*(101-0)+0);
     
-    console.log(index);
+    console.log("inex is "+index);
     console.log(users[index]);
 
     decAtrChance (index)
     
-    console.log(users[index].atrChance);     
+    console.log("atrChanse after dec "+users[index].atrChance);     
     console.log('compare is '+compareAtr);
 }
-
-getRandomUser()
